6b4e197c7f754cf3d2180fd0b4f15874c38e552e
2021000372PC <jhyou8912@gmail.com>
2021-12-15 14:11:12 +0900

    bootstrap 적용, 메인 페이지 설정

--- z_diary_proj/urls.py
+++ z_diary_proj/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import common_app.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', common_app.views.index),
 ]
